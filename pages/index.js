8889c55f4d7803aec8893dd07058cecfc8f51794
Rudiney de Assis <rudiney.assis@gmail.com>
2022-04-20 08:47:54 -0300

    integração da home + slugify

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+import slugify from 'slugify'
 import {
     Box,
     Container,
@@ -8,12 +10,19 @@ import {
     Typography
 } from '@material-ui/core'
 
+
 import { makeStyles } from '@material-ui/core/styles'
 import SearchIcon from '@material-ui/icons/Search'
 import TemplateDefault from '../src/templates/Default'
 import Card from '../src/components/Card'
+import dbConnect from '../src/utils/dbConnect'
+import ProductsModel from '../src/models/products'
+import { formatCurrency } from '../src/utils/currency'
 
 const useStyles = makeStyles((theme) => ({
+    productLink: {
+        textDecoration: 'none !important'
+    },
     searchBox: {
         display: 'flex',
         justifyContent: 'center',
@@ -25,7 +34,7 @@ const useStyles = makeStyles((theme) => ({
     }
 }))
 
-const Home = () => {
+const Home = ({ products }) => {
 
     const classes = useStyles()
 
@@ -52,32 +61,44 @@ const Home = () => {
                 </Typography>
                 <br />
                 <Grid container spacing={4}>
-                    <Grid item xs={12} sm={6} md={4}>
-                    <Card 
-                        image={'https://source.unsplash.com/random'}
-                        title="Produto X"
-                        subtitle="R$ 60,00"
-                    />
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={4}>
-                    <Card 
-                        image={'https://source.unsplash.com/random'}
-                        title="Produto X"
-                        subtitle="R$ 60,00"
-                    />
-                    </Grid>
+                    {
+                        products.map(product => {
+                            const category = slugify(product.category).toLocaleLowerCase()
+                            const title = slugify(product.title).toLocaleLowerCase()
 
-                    <Grid item xs={12} sm={6} md={4}>
-                    <Card 
-                        image={'https://source.unsplash.com/random'}
-                        title="Produto X"
-                        subtitle="R$ 60,00"
-                    />
-                    </Grid>
+                            return (
+                                <Grid key={product._id} item xs={12} sm={6} md={4}>
+                                    <Link href={`/${category}/${title}/${product._id}`} passHref>
+                                        <a className={classes.productLink}>
+                                            <Card 
+                                                image={`/uploads/${product.files[0].name}`}
+                                                title={product.title}
+                                                subtitle={formatCurrency(product.price)}
+                                            />
+                                        </a>
+                                    </Link>
+                                </Grid>
+                            )
+                        })
+                    }
                 </Grid>
             </Container>
         </TemplateDefault>
     )
+}
+
+export async function getServerSideProps() {
+    await dbConnect()
+
+    const products = await ProductsModel.aggregate([{
+        $sample: { size: 6 }
+    }])
+
+    return {
+        props: {
+            products: JSON.parse(JSON.stringify(products))
+        }
+    }
 }
 
 export default Home
